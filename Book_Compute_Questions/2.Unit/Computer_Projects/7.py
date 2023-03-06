961f97d76765f6c280e91cdbf40c15208cb9aece
alhas <bahtiyaralialhas@gmail.com>
2023-03-06 23:22:04 +0100

    figured out how to make multiplication. Trying to implement in for loop

--- Book_Compute_Questions/2.Unit/Computer_Projects/7.py
+++ Book_Compute_Questions/2.Unit/Computer_Projects/7.py
@@ -1,7 +1,7 @@
 from random import randint
 
 
-""" 
+"""
 1. m X k matrix A ------ 2 X 3
 2. k X n matrix B ------ 3 X 4
 
@@ -10,14 +10,19 @@ from random import randint
 
 def matrix_multi():
     A = [[randint(0, 5) for _ in range(3)] for _ in range(2)]
-    B = [[randint(1, 6)for _ in range(4)] for _ in range(3)]
+    B = [[randint(1, 6) for _ in range(4)] for _ in range(3)]
     for i in range(len(B)):
         try:
             print(f"{A[i]}\u0020\u0020{B[i]}")
-        
+
         except IndexError:
             print(f"\t   {B[i]}")
 
+    for i in A[0][1]:
+        print(i)
+
+
+# print(f'{A[0][0]*B[0][0] + A[0][1]*B[1][0] + A[0][2]*B[2][0]}')
 # Complation need for first row first column calculation.
 
 
