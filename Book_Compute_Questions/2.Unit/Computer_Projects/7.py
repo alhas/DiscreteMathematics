1f505bfbe762a8f85c41d0ce56da9cf0fac9c0a2
alhas <bahtiyaralialhas@gmail.com>
2023-03-10 23:22:37 +0100

    latest

--- Book_Compute_Questions/2.Unit/Computer_Projects/7.py
+++ Book_Compute_Questions/2.Unit/Computer_Projects/7.py
@@ -1,44 +1,35 @@
 from random import randint
 
-
 """
 1. m X k matrix A ------ 2 X 3
 2. k X n matrix B ------ 3 X 4
-
 """
 
 
 def matrix_multi():
-    A = [[randint(0, 5) for _ in range(3)] for _ in range(2)]
-    B = [[randint(1, 6) for _ in range(4)] for _ in range(3)]
-    for i in range(len(B)):
+    A = [[randint(0, 9) for _ in range(3)] for _ in range(2)]
+    B = [[randint(0, 9) for _ in range(4)] for _ in range(3)]
+    for i_row in range(len(B)):
         try:
-            print(f"{A[i]}\u0020\u0020{B[i]}")
+            print(f"{A[i_row]}\u0020\u0020{B[i_row]}")
 
         except IndexError:
-            print(f"\t   {B[i]}")
-    value_a = list()
-    value_b = list()
-    index_a = 0
-    for row in A:
-        for x in range(len(row)):
-            value_a.append(A[index_a][x])
-        index_a += 1
+            print(f"\t   {B[i_row]}")
+    value_a = int()
+    value_b = int()
 
-    for column in range(len(B[0])):
-        for i in range(len(B)):
-            # value_b = B[i][index_b]
-            value_b.append(B[i][column])
+    for x_row in range(len(A)):
+        for x_column in range(len(A[0])):
+            value_a = A[x_row][x_column]
+    for y_column in range(len(B[0])):
+        for i_row in range(len(B)):
+            value_b = B[i_row][y_column]
 
     print(value_a, value_b)
-
-
 
 
 # Index Error on B
 
 # print(f'{A[0][0]*B[0][3] + A[0][1]*B[1][3] + A[0][2]*B[2][3]}')
 # Complation need for first row first column calculation.
-
-
 matrix_multi()
